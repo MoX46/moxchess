6b064365a903c688e9e613387fbe9cdaf4068be4
mox46 <m.ahmed.64@gmail.com>
2022-11-20 00:44:42 -0500

    fen info decoder, pieces and turn to property

--- game/game.py
+++ game/game.py
@@ -1,15 +1,45 @@
 import re
-from game.gamepiece import GamePiece, Piece, Color, Square
 from enum import Enum
+from game.gamepiece import GamePiece, Piece, Color, Square
+
+def fen_to_game_info(fen:str) -> dict:
+    return_value = {
+        'side_to_move':Color.NONE,
+        'castling_ability':{}
+    }
+    fen_array = fen.split(' ')
+    side_to_move = fen_array[1]
+    castling_ability = fen_array[2]
+    ep_square = fen_array[3]
+    halfmove_clock = fen_array[4]
+    fullmove_clock = fen_array[5]
+
+    if side_to_move == 'w':
+        return_value['side_to_move'] = Color.WHITE    
+    else:
+        return_value['side_to_move'] = Color.BLACK
+
+    if 'K' in castling_ability:
+        return_value['castling_ability'][Color.WHITE] = 64
+    if 'K' in castling_ability:
+        return_value['castling_ability'][Color.WHITE] = 56
+    if 'k' in castling_ability:
+        return_value['castling_ability'][Color.BLACK] = 8
+    if 'q' in castling_ability:
+        return_value['castling_ability'][Color.BLACK] = 1
+
+    return return_value
 
 def fen_to_pieces(fen:str) -> list[GamePiece]:
     """Converts a FEN string with a list of piece objects which can be added to the board object"""
     piece_list = []
     fen_entry_to_piece = {
-        'r':(Piece.ROOK,Color.BLACK), 'n':(Piece.KNIGHT,Color.BLACK), 'b':(Piece.BISHOP,Color.BLACK),
-        'q':(Piece.QUEEN,Color.BLACK), 'k':(Piece.KING,Color.BLACK), 'p':(Piece.PAWN,Color.BLACK),
-        'R':(Piece.ROOK,Color.WHITE), 'N':(Piece.KNIGHT,Color.WHITE), 'B':(Piece.BISHOP,Color.WHITE),
-        'Q':(Piece.QUEEN,Color.WHITE), 'K':(Piece.KING,Color.WHITE), 'P':(Piece.PAWN,Color.WHITE),
+        'r':(Piece.ROOK,Color.BLACK), 'n':(Piece.KNIGHT,Color.BLACK),
+        'b':(Piece.BISHOP,Color.BLACK), 'q':(Piece.QUEEN,Color.BLACK),
+        'k':(Piece.KING,Color.BLACK), 'p':(Piece.PAWN,Color.BLACK),
+        'R':(Piece.ROOK,Color.WHITE), 'N':(Piece.KNIGHT,Color.WHITE),
+        'B':(Piece.BISHOP,Color.WHITE), 'Q':(Piece.QUEEN,Color.WHITE),
+        'K':(Piece.KING,Color.WHITE), 'P':(Piece.PAWN,Color.WHITE)
     }
 
     counter = 0
@@ -19,9 +49,9 @@ def fen_to_pieces(fen:str) -> list[GamePiece]:
         if c == '/':
             continue
         if re.search('[0-9]',c):
-            counter = counter + int(c)# skip columns when a number is present, loop back to 0 after 7
+            counter += int(c)# skip columns when a number is present, loop back to 0 after 7
             continue
-        counter = counter + 1
+        counter += 1
         piece, color = fen_entry_to_piece[c]
         piece_list.append(GamePiece(Square(counter),piece,color))
     return piece_list
@@ -46,23 +76,24 @@ def is_fen_valid(fen:str) -> bool:
             return False
     return True
 
-class CastlingDirection(Enum):
-    KING_SIDE = 0
-    QUEEN_SIDE = 1
-
 class Game():
     def __init__(self, fen:str = ''):
-        if type(fen) != str:
+        if not isinstance(fen, str):
             raise TypeError('FEN should be a string')
         if not is_fen_valid(fen):
             raise ValueError('Invalid FEN')
-        self._pieces = fen_to_pieces(fen)
-        self._turn = Color.WHITE
+        self.fen = fen
+        self._pieces = None
+        self._turn = fen_to_game_info(fen)['side_to_move']
         self._castling_abilty = {}
 
+    @property
     def pieces(self) -> list[GamePiece]:
+        if self._pieces is None:
+            self._pieces = fen_to_pieces(self.fen)
         return self._pieces
 
+    @property
     def turn(self) -> Color:
         return self._turn
     
---
+++ tests/test_game.py
@@ -0,0 +1,35 @@
+import unittest
+from game.game import Game
+from game.gamepiece import GamePiece, Piece, Color, Square
+
+class TestGamePiece(unittest.TestCase):
+    """Tests the game piece class """
+    def setUp(self) -> None:
+        self.game1 = Game('7q/2P1b3/6n1/1Q3B2/2N3k1/p3K3/8/6Rr w - - 0 1')
+        self.game2 = Game('2n2b2/r1pK4/P2P1P2/q7/4pNB1/1bk5/5p2/7N b - - 0 1')
+
+    def test_pieces(self) -> None:
+        """Tests piece identity method"""
+        expected_list = [
+            GamePiece(Square.H8,Piece.QUEEN,Color.BLACK),
+            GamePiece(Square.C7,Piece.PAWN,Color.WHITE),
+            GamePiece(Square.E7,Piece.BISHOP,Color.BLACK),
+            GamePiece(Square.G6,Piece.KNIGHT,Color.BLACK),
+            GamePiece(Square.B5,Piece.QUEEN,Color.WHITE),
+            GamePiece(Square.F5,Piece.BISHOP,Color.WHITE),
+            GamePiece(Square.C4,Piece.KNIGHT,Color.WHITE),
+            GamePiece(Square.G4,Piece.KING,Color.BLACK),
+            GamePiece(Square.A3,Piece.PAWN,Color.BLACK),
+            GamePiece(Square.E3,Piece.KING,Color.WHITE),
+            GamePiece(Square.G1,Piece.ROOK,Color.WHITE),
+            GamePiece(Square.H1,Piece.ROOK,Color.BLACK)
+        ]
+        self.assertEqual(expected_list,self.game1.pieces)
+
+    def test_turn(self) -> None:
+        """Test if game returns correct turn"""
+        self.assertEqual(Color.WHITE,self.game1.turn)
+        self.assertEqual(Color.BLACK,self.game2.turn)
+
+if __name__ == '__main__':
+    unittest.main()
